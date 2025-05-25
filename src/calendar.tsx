10362451ad79e2378dcf521b7e0b45791b508374
darealLord <133997594+anonygrammer69@users.noreply.github.com>
2025-05-25 11:54:53 +0530

    Update calendar.tsx

--- src/calendar.tsx
+++ src/calendar.tsx
@@ -213,7 +213,7 @@ const Calendar: React.FC = () => {
         day = addDays(day, 1);
       }
       days.push(<div className="flex flex-auto aspect-auto content=width=device-width p-5 justify-evenly bg-yellow-600
-         text-black border-3 rounded-lg border-black w-170 h-full relative" key={day.toString()}>{week}</div>);
+         text-black border-3 rounded-lg border-black w-full h-full relative" key={day.toString()}>{week}</div>);
     }
     return <div className="calendar">{days}</div>;
   };
