31045bd8574253352de67235e7733b98c3079a2b
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-05-25 15:29:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
       <h1 className="flex justify-center text-sm lg:mt-6 lg:ml-20 lg:text-lg underline font-semibold text-gray-800">Welcome to your Calendar</h1>
       <div>
         <a>
-          <img src={CALENDAR} className="flex flex-row h-20 justify-center ml-70 lg:ml-125 my-6 hover:shadow-2xl hover:shadow-amber-500 transition duration-300 ease-in-out"/>
+          <img src={CALENDAR} className="flex flex-row h-20 justify-center ml-70 lg:ml-125 my-6 hover:shadow-2xl hover:shadow-amber-700 transition duration-300 ease-in-out"/>
         </a>
       </div>
       <div>
