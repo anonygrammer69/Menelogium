145ec33b2cb40f98f11a4fa80eafd072edb894ae
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-05-25 20:19:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       >
         Logout
       </button>
-      <h1 className="flex text-base justify-center ml-20 mt-10 sm:mt-10 md:mt-10 md:ml-10 underline font-semibold md:font-semibold text-gray-800">Welcome to your Calendar</h1>
+      <h1 className="flex text-base justify-center ml-16 mt-10 sm:mt-10 md:mt-10 md:ml-10 underline font-semibold md:font-semibold text-gray-800">Welcome to your Calendar</h1>
       <div>
         <a>
           <img src={CALENDAR} className="flex h-20 justify-center sm:ml-75 md:ml-125 my-6"/>
