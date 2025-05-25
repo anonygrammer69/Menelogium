54c1d1e8a76348a85692393619ed9e435ca0a939
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-05-25 19:28:01 +0530

    ui update

--- src/App.tsx
+++ src/App.tsx
@@ -22,15 +22,15 @@ function App() {
   return (
     <>
       <button
-        className="absolute top-8 right-4 lg:right-16 bg-red-500 text-white hover:bg-red-700 hover:cursor-pointer px-4 py-2 rounded"
+        className="absolute top-8 sm:right-0 md:right-16 bg-red-500 text-white hover:bg-red-700 hover:cursor-pointer px-4 py-2 rounded"
         onClick={() => signOut(auth)}
       >
         Logout
       </button>
-      <h1 className="flex justify-center text-sm lg:mt-6 lg:ml-20 lg:text-lg underline font-semibold text-gray-800">Welcome to your Calendar</h1>
+      <h1 className="flex justify-center underline font-semibold text-gray-800">Welcome to your Calendar</h1>
       <div>
         <a>
-          <img src={CALENDAR} className="flex h-20 justify-center ml-70 lg:ml-125 my-6 hover:shadow-2xl hover:shadow-amber-700 transition duration-300 ease-in-out"/>
+          <img src={CALENDAR} className="flex h-20 justify-center ml-70 sm:ml-90 md:ml-125 my-6"/>
         </a>
       </div>
       <div>
