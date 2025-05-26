a4e050492af563b82754ba6b02605e09d09dfdf7
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-05-26 16:22:18 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <>
       <button
-        className="absolute top-8 right-4 sm:right-2 md:right-16 bg-red-500 text-white hover:bg-red-600 hover:shadow-xl hover:shadow-red-700 transition duration-300 hover:cursor-pointer px-4 py-2 rounded"
+        className="absolute top-8 right-2 sm:right-2 md:right-16 bg-red-500 text-white hover:bg-red-600 hover:shadow-xl hover:shadow-red-700 transition duration-300 hover:cursor-pointer px-4 py-2 rounded"
         onClick={() => signOut(auth)}
       >
         Logout
