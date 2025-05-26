1d21410aa44926a6e2aabd6c7628b7bf44dfa6b1
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-05-26 17:49:18 +0530

    bg color change

--- src/calendar.tsx
+++ src/calendar.tsx
@@ -313,7 +313,7 @@ const renderSelectedMonthEvents = () => {
   return (
     <>
       {dialogBox}
-      <div className="flex flex-col md:flex-row md:ml-10">
+      <div className="flex flex-col md:flex-row">
         <div className="table">
           {renderHeader()}
           {renderDays()}
