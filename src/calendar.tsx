63748aec90d5597ea4d222b7077ba1bbbf6f0067
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-05-28 15:42:35 +0530

    updates

--- src/calendar.tsx
+++ src/calendar.tsx
@@ -325,7 +325,7 @@ return (
     <>
       {dialogBox}
       <button 
-        className="fixed top-4 left-4 justify-start z-40 bg-blue-600 hover:bg-blue-700 transition border-2 border-black rounded-lg w-18 h-14 text-white"
+        className="fixed top-4 left-4 z-40 bg-blue-600 hover:bg-blue-700 transition border-2 border-black rounded-lg w-18 h-14 text-white"
         onClick={() => setShowSidebar((prev) => !prev)}
       >
         {showSidebar ? "Hide Menu" : "Show Menu"}
@@ -335,8 +335,8 @@ return (
           <div 
             className={`
               fixed top-0 left-0 z-40 w-64 h-full bg-gradient-to-b from-white to-slate-400 border-2 border-black rounded-r-lg p-4 gap-4 flex flex-col shadow-lg
-              transition-transform duration-500 ease-in-out
-              ${showSidebar ? "translate-x-0" : "-translate-x-full"}
+              sidebar-transition
+              ${showSidebar ? "opacity-100" : "opacity-0 pointer-events-none"}
             `}
             style={{ willChange: "transform" }}
           >
