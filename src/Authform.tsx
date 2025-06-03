80d4998349d23aa16e42c61c6707aa08180e254c
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-06-03 23:53:51 +0530

    Update Authform.tsx

--- src/Authform.tsx
+++ src/Authform.tsx
@@ -66,7 +66,7 @@ const AuthForm: React.FC<{ onAuth: () => void }> = ({ onAuth }) => {
       </button>
       <button
         type="button"
-        className="bg-gray-300 text-black rounded p-2 hover:bg-gray-400 mt-4 w-52 h-16"
+        className="bg-gray-300 text-black justify-center rounded p-2 hover:bg-gray-400 mt-4"
         onClick={handleGoogleSignIn}
       >
         <img src={googleLogo} alt="Google" className="h-6 w-6 justify-center mr-2" /> Sign in with Google
