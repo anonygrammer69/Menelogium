7d2f1f094ee6a04889484856db8a2ad7d2f3eb5a
anonygrammer69 <133997594+anonygrammer69@users.noreply.github.com>
2025-06-07 16:17:11 +0530

    updates

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -7,22 +7,7 @@ module.exports = {
     extend: {
       fontfamily: {
         garamond: ["garamond", "serif"]
-      },
-      colors: {
-        'beige': {
-          '50': '#fcf8ef',
-          '100': '#f7efdc',
-          '200': '#e9e0c0',
-          '300': '#d9d097',
-          '400': '#c9c07d',
-          '500': '#b9b064',
-          '600': '#a99f4b',
-          '700': '#8b7f33',
-          '800': '#6d6128',
-          '900': '#4d421c',
-          '950': '#352d14',
-        },
-      },
+      }
     },
   plugins: [],
   }
